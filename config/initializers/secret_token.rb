eb58c5a07f7eadb631c6d9c1a2df07360356c8d3
Justin Paulson <justinapaulson@gmail.com>
2014-01-24 00:50:24 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Justinpaulsoncom::Application.config.secret_key_base = 'dc13c6166bce4127031683ee2b710646a7971933e91c19349c882dfff97ebf6f80143f3356028057767791b99ef073511ad4a198572beef2fef2dbb01f166760'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Justinpaulsoncom::Application.config.session_store :cookie_store, key: '_justinpaulsoncom_session'
